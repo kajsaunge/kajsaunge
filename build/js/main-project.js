395fc7493db4c3e8ab93441b7d7755a6f5c1150c
Kajsa Unge <hej@kajsaunge.se>
2017-09-10 15:51:15 +0200

    Removes jQuery on nav actions from project pages, ie, adds anchors and fade class to move smoothly to relevnat position on index page.

--- build/js/main-project.js
+++ build/js/main-project.js
@@ -1,16 +1,41 @@
-$(function(){
-	$('.about').click(function(){
-		sessionStorage.setItem('anchor', 'about');
-	});
+document.addEventListener('DOMContentLoaded', function() {
+		var navItemAbout = document.getElementsByClassName('about')[1];
+		var navItemCv = document.getElementsByClassName('cv')[1];
+		var navItemFade = document.getElementsByClassName('hide-fade-html');
 
-	$('.cv').click(function(){
-		sessionStorage.setItem('anchor', 'cv');
-	});
+		navItemAbout.addEventListener('click', function() {
+			sessionStorage.setItem('anchor', 'about');
+		});
 
-	$('.hide-fade-html').click(function(){
-		var location = this.href;
-		event.preventDefault();
-		sessionStorage.setItem('navFromProject', 'true');
-		window.location = location;
-	});
+		navItemCv.addEventListener('click', function() {
+			sessionStorage.setItem('anchor', 'cv');
+		});
+
+		for(var i = 0; i < navItemFade.length; i++) {
+			navItemFade[i].addEventListener('click', function() {
+				var location = this.href;
+
+				event.preventDefault();
+				sessionStorage.setItem('navFromProject', 'true');
+				window.location = location;
+			});
+		}
 });
+
+// chubby old jQuery
+// $(function(){
+	// $('.about').click(function(){
+	// 	sessionStorage.setItem('anchor', 'about');
+	// });
+
+// 	$('.cv').click(function(){
+// 		sessionStorage.setItem('anchor', 'cv');
+// 	});
+
+// 	$('.hide-fade-html').click(function(){
+// 		var location = this.href;
+// 		event.preventDefault();
+// 		sessionStorage.setItem('navFromProject', 'true');
+// 		window.location = location;
+// 	});
+// });
