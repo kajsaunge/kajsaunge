945d5c26d7f4f32fba5f3c77deac2ec291b98589
Kajsa Unge <hej@kajsaunge.se>
2017-09-10 13:10:27 +0200

    Remove jQuery for projects navigation, to hide the active project.

--- build/js/project-active.js
+++ build/js/project-active.js
@@ -1,4 +1,10 @@
-$(function(){
+document.addEventListener('DOMContentLoaded', function() {
 	var id = parent.document.URL.split('=');
-	$('#project-' + id[1]).addClass('project-active');
+	document.getElementById('project-' + id[1]).className +=' project-active';
 });
+
+// chubby old jQuery
+// $(function(){
+// 	var id = parent.document.URL.split('=');
+// 	$('#project-' + id[1]).addClass('project-active');
+// });
