fec39c9f3168f22ded80fcac5bd5fd9e69ab3391
Kajsa Unge <hej@kajsaunge.se>
2017-09-24 07:47:36 +0200

    Removes all jQuery and duplicate navigation for project pages. Adds fixes for navigation reuse to work on both home and project pages.

--- build/js/main.js
+++ build/js/main.js
@@ -1,28 +1,19 @@
-$(function(){
-	if (sessionStorage.getItem('navFromProject') === 'true'){
-		$('html').addClass('hide-html');
-		$('html').fadeIn(100);
+document.addEventListener('DOMContentLoaded', function() {
 
-		if (sessionStorage.getItem('anchor') === 'about' || sessionStorage.getItem('anchor') === 'cv'){
-			var offsetSize = setScrollOffsetSize($(window).width(), sessionStorage.getItem('anchor'));
-		    window.scrollTo(0, sessionStorage.getItem('anchor') === 'about' ? $('#about').offset().top - offsetSize : $('#cv').offset().top - offsetSize);
-		}
-
-		sessionStorage.removeItem('navFromProject');
-		sessionStorage.removeItem('anchor');
-	} else {
-		$('html').removeClass('hide-html');
+	// hide current active project
+	if (parent.document.URL.includes('?')) {
+		var id = parent.document.URL.split('=');
+		document.getElementById('project-' + id[1]).className += ' project-active';
 	}
-});
 
-function setScrollOffsetSize(width, fromPage){
-		if (width >= 1660) {
-		    return fromPage === 'about' ? 750 : 110;
-		} else if (width >= 1000 && width <= 1659) {
-		    return fromPage === 'about' ? 460 : 100;
-		} else if (width >= 500 && width <= 999) {
-		    return fromPage === 'about' ? 510 : 120;
-		} else {
-		    return fromPage === 'about' ? 260 : 95;
-		}
-} 
+/* Pseudo smooth scroll
+	1) Click on navItem
+	2) Get current position
+	3) Get destination position
+	4) Calculate distance to always get same speed
+	5) Set easing at some point
+	6) Move untill reached destination
+
+*/
+
+});
