1d63a74c2a55f5f3eeb8307f8cd98987082a9e5a
Kajsa Unge <hej@kajsaunge.se>
2017-09-30 15:36:22 +0200

    SmoothScroll process - got PZ help.

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+"use strict";
 
 	// hide current active project
 	if (parent.document.URL.includes('?')) {
@@ -6,14 +7,93 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('project-' + id[1]).className += ' project-active';
 	}
 
-/* Pseudo smooth scroll
-	1) Click on navItem
-	2) Get current position
-	3) Get destination position
-	4) Calculate distance to always get same speed
-	5) Set easing at some point
-	6) Move untill reached destination
 
-*/
+		var mainNav = document.getElementById('main-nav');
+		var navElms = mainNav.getElementsByTagName('a');
+
+		var n = navElms.length;
+
+		var menuHeight = mainNav.offsetHeight;
+
+		for(var i = 0; i<n; i++){
+			var navElm = navElms[i];
+			navElm.addEventListener('click', function(event) {
+
+				var startLocation = window.pageYOffset;
+				//console.log(startLocation, 'startLocation');
+
+				var clickedElTarget = this.href.split('#')[1];
+				var destinationId = document.getElementById(clickedElTarget);
+				var endLocation = destinationId.offsetTop;
+				//console.log(endLocation, 'endLocation');
+
+				var distance = 0
+
+				if (endLocation < startLocation) {
+					distance = endLocation - startLocation;
+					// distance = endLocation - -startLocation;
+					console.log('endlocation is smaller', distance);
+				} else {
+					distance = endLocation + startLocation;
+					console.log('endlocation is BIGGER', distance);
+				}
+
+
+
+				var increments = (distance / 62);
+				if ( increments >= 0 ) {
+				                // Stop animation when you reach the anchor OR the bottom of the page
+				                var stopAnimation = function () {
+				                    var travelled = window.pageYOffset;
+				                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+				                        clearInterval(runAnimation);
+				                    }
+				                };
+				            }
+				// var stopAnimation = function () {
+				//
+				// 		var travelled = window.pageYOffset;
+				// 		if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+				// 				clearInterval(runAnimation);
+				// 		}
+				// 		// if (endLocation > travelled) {
+				// 		// 	console.log(travelled, 'traveling down');
+				// 		// 	if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+				// 		// 			clearInterval(runAnimation);
+				// 		// 	}
+				// 		// } else {
+				// 		// 	console.log(travelled, 'traveling up');
+				// 		// 	clearInterval(runAnimation);
+				// 		// }
+				// };
+
+				var animateScroll = function () {
+					window.scrollBy(0, increments);
+					stopAnimation();
+				};
+
+			  var runAnimation = setInterval(animateScroll, 16);
+
+				event.preventDefault();
+			});
+			var scrollToggle = document.querySelectorAll('.scroll');
+        // For each smooth scroll link
+        [].forEach.call(scrollToggle, function (toggle) {
+            // When the smooth scroll link is clicked
+            toggle.addEventListener('click', function(e) {
+                // Prevent the default link behavior
+                e.preventDefault();
+                // Get anchor link and calculate distance from the top
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+                var dataSpeed = toggle.getAttribute('data-speed');
+                // If the anchor exists
+                if (dataTarget) {
+                    // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 500);
+                }
+            }, false);
+        });
+		}
 
 });
