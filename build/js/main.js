e5d1eafff7c682f9f924dfbbdde0d8e3af437cbb
Kajsa Unge <hej@kajsaunge.se>
2017-10-01 11:14:44 +0200

    Tidying up.

--- build/js/main.js
+++ build/js/main.js
@@ -7,100 +7,58 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('project-' + id[1]).className += ' project-active';
 	}
 
+	// smoothScroll
 	var mainNav = document.getElementById('main-nav');
 	var navElms = mainNav.getElementsByTagName('a');
 	var n = navElms.length;
 	var menuHeight = mainNav.offsetHeight;
 
-		for(var i = 0; i < n; i++){
-			var navElm = navElms[i];
-			navElm.addEventListener('click', function(event) {
+	for(var i = 0; i < n; i++){
+		var navElm = navElms[i];
 
-				var startLocation = window.pageYOffset;
-				// #1 console.log(startLocation, 'startLocation');
+		navElm.addEventListener('click', function(event) {
+			var startLocation = window.pageYOffset;
+			var clickedElTarget = this.href.split('#')[1];
+			var destinationId = document.getElementById(clickedElTarget);
+			var endLocation = destinationId.offsetTop;
+			var distance = 0
 
-				var clickedElTarget = this.href.split('#')[1];
-				var destinationId = document.getElementById(clickedElTarget);
-				var endLocation = destinationId.offsetTop;
-				// #2 console.log(endLocation, 'endLocation');
-				var distance = 0
-
-
-				if (endLocation < startLocation) {
-					distance = endLocation - startLocation;
-					// #3 distance = endLocation - -startLocation;
-					console.log('endlocation is smaller', distance);
-				} else {
+			if (endLocation < startLocation) {
+				distance = endLocation - startLocation;
+			} else {
 					distance = endLocation + startLocation;
-					console.log('endlocation is BIGGER', distance);
+			}
+
+			var increments = (distance / 62);
+			var stopAnimation;
+
+			if ( endLocation >= startLocation ) {
+        stopAnimation = function () {
+          var travelled = window.pageYOffset;
+
+			    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+            clearInterval(runAnimation);
+          }
 				}
-
-
-
-				var increments = (distance / 62);
-				var stopAnimation;
-				if ( endLocation >= startLocation ) {
-            stopAnimation = function () {
-                var travelled = window.pageYOffset;
-                if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-									console.log('travelled >=', document.body.offsetHeight);
-                    clearInterval(runAnimation);
-                }
-						}
-				} else {
+			} else {
 					stopAnimation = function () {
-							var travelled = window.pageYOffset;
-							if ( (travelled <= (endLocation - increments)) || ((window.innerHeight - travelled) >= document.body.offsetHeight) ) {
-								console.log('travelled <= HEEEEEJE - never stops', document.body.offsetHeight);
-									clearInterval(runAnimation);
-							}
+						var travelled = window.pageYOffset;
+
+						if ( (travelled <= (endLocation - increments)) || ((window.innerHeight - travelled) >= document.body.offsetHeight) ) {
+							clearInterval(runAnimation);
+						}
 					}
 				}
-				// var stopAnimation = function () {
-				//
-				// 		var travelled = window.pageYOffset;
-				// 		if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-				// 				clearInterval(runAnimation);
-				// 		}
-				// 		// if (endLocation > travelled) {
-				// 		// 	console.log(travelled, 'traveling down');
-				// 		// 	if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-				// 		// 			clearInterval(runAnimation);
-				// 		// 	}
-				// 		// } else {
-				// 		// 	console.log(travelled, 'traveling up');
-				// 		// 	clearInterval(runAnimation);
-				// 		// }
-				// };
 
-				var animateScroll = function () {
-					window.scrollBy(0, increments);
-					console.log('scrollBy increments:', increments);
-					stopAnimation();
-				};
+			var animateScroll = function () {
+				window.scrollBy(0, increments);
+				stopAnimation();
+			};
 
-			  var runAnimation = setInterval(animateScroll, 16);
+		  var runAnimation = setInterval(animateScroll, 16);
 
-				event.preventDefault();
-			});
-			// var scrollToggle = document.querySelectorAll('.scroll');
-        // For each smooth scroll link
-        // [].forEach.call(scrollToggle, function (toggle) {
-        //     // When the smooth scroll link is clicked
-        //     toggle.addEventListener('click', function(e) {
-        //         // Prevent the default link behavior
-        //         e.preventDefault();
-        //         // Get anchor link and calculate distance from the top
-        //         var dataID = toggle.getAttribute('href');
-        //         var dataTarget = document.querySelector(dataID);
-        //         var dataSpeed = toggle.getAttribute('data-speed');
-        //         // If the anchor exists
-        //         if (dataTarget) {
-        //             // Scroll to the anchor
-        //             smoothScroll(dataTarget, dataSpeed || 500);
-        //         }
-        //     }, false);
-        // });
-		}
+			event.preventDefault();
+		});
+	}
 
 });
