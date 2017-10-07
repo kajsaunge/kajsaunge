ff2364d09a68afc998c73d7b3e1650af7b53a05b
Kajsa Unge <hej@kajsaunge.se>
2017-10-07 23:14:16 +0200

    Removes refactored comments.

--- build/js/main.js
+++ build/js/main.js
@@ -2,21 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
 "use strict";
 
 	// hide current active project
-	// if (parent.document.URL.includes('?')) {
-	//  const id = parent.document.URL.split('=');
-	// 	document.getElementById('project-' + id[1]).className += ' project-active';
-	// }
-	// ES6 verion of above outcommented
 	const activeProject = parent.document.URL.split('=')[1];
 	const hideActiveProject = parent.document.URL.includes('?') ?
 		document.getElementById('project-' + activeProject).className += ' project-active' : ''
 
-	// smoothScroll
 	const mainNav = document.getElementById('main-nav');
 	const navElms = mainNav.getElementsByTagName('a');
 	const n = navElms.length;
 	const menuHeight = mainNav.offsetHeight;
 
+	// smoothScroll
 	for(let i = 0; i < n; i++){
 		const navElm = navElms[i];
 
@@ -26,55 +21,15 @@ document.addEventListener('DOMContentLoaded', function() {
 			const endLocation = document.getElementById(clickedElAnchor).offsetTop;
 			const distance = endLocation - startLocation;
 			const frames = 16;
-			let windowHeight = window.innerHeight
-			let bodyHeight = document.body.offsetHeight;
-
-			// Account for the menue when scrolling
-			// let adjustedEndLocation;
-			// if (clickedElAnchor === 'page-top') {
-			// 	adjustedEndLocation = endLocation;
-			// }
-			// else if (distance < 0) {
-			// 	adjustedEndLocation = endLocation - (menuHeight*2)
-			// }
-			// else {
-			// 	adjustedEndLocation = endLocation - menuHeight
-			// }
-			// ES6 verion of above outcommented - not sure its better as it takes more cognitive effort to read
 			const adjustedEndLocation = distance < 0
 				? (clickedElAnchor === 'page-top' ? endLocation : endLocation - (menuHeight*2))
 				: endLocation - menuHeight;
 
-			// var speed;
-			// // Adjust the speed
-			// if (distance < 2000 && distance > -2000) {
-			// 	speed = 500;
-			// } else {
-			// 	speed = 1000;
-			// }
-			// ES6 verion of above outcommented
 			const speed = distance < 2000 && distance > -2000 ? 500 : 1000;
-
 			const increments = (distance/(speed/frames));
+			let windowHeight = window.innerHeight;
+			let bodyHeight = document.body.offsetHeight;
 
-			// let stopAnimation;
-			//
-			// if ( adjustedEndLocation >= startLocation ) {
-      //   stopAnimation = () => {
-			// 		let pageYOffset = window.pageYOffset;
-			//     if ( (pageYOffset >= (adjustedEndLocation - increments)) || ((windowHeight + pageYOffset) >= bodyHeight) ) {
-			// 			clearInterval(runAnimation);
-      //     }
-			// 	}
-			// } else {
-			// 		stopAnimation = () => {
-			// 			let pageYOffset = window.pageYOffset;
-			// 			if ( (pageYOffset <= (adjustedEndLocation - increments)) || ((windowHeight - pageYOffset) >= bodyHeight) ) {
-			// 				clearInterval(runAnimation);
-			// 			}
-			// 	}
-			// }
-			// ES6 verion of above outcommented
 			let stopAnimation = () => {
 				let pageYOffset = window.pageYOffset;
 				if ( adjustedEndLocation >= startLocation ) {
